5cee300bb45e7e73b1664571c803cae222f6608e
Jonathan Colon <jonatron2.0@gmail.com>
2017-01-08 16:38:55 -0500

    inital Meme class created

---
+++ app/src/main/java/nyc/c4q/jonathancolon/dankify/Meme.java
@@ -0,0 +1,12 @@
+package nyc.c4q.jonathancolon.dankify;
+
+/**
+ * Created by jonathancolon on 1/8/17.
+ */
+
+public class Meme {
+    public String topText;
+    public String bottomText;
+    byte[] memeImage;
+    String dateCreated;
+}
