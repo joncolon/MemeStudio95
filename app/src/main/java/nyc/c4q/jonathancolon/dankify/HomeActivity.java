3a0bbaf84fb5526f658ad216047244fde066089d
Jonathan Colon <jonatron2.0@gmail.com>
2017-01-08 17:53:13 -0500

    added meme sqlite database

--- app/src/main/java/nyc/c4q/jonathancolon/dankify/HomeActivity.java
+++ app/src/main/java/nyc/c4q/jonathancolon/dankify/HomeActivity.java
@@ -1,9 +1,12 @@
 package nyc.c4q.jonathancolon.dankify;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/nyc/c4q/jonathancolon/dankify/sqlite/SqlHelper.java
@@ -0,0 +1,40 @@
+package nyc.c4q.jonathancolon.dankify.sqlite;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import nl.qbusict.cupboard.QueryResultIterable;
+import nyc.c4q.jonathancolon.inContaq.contactlist.Contact;
+
+import static nl.qbusict.cupboard.CupboardFactory.cupboard;
+
+
+public class SqlHelper {
+
+    public SqlHelper() {
+    }
+
+    public static List<Contact> selectAllContacts(SQLiteDatabase db) {
+        List<Contact> contacts = new ArrayList<>();
+        try {
+            QueryResultIterable<Contact> itr = cupboard().withDatabase(db).query(Contact.class).query();
+            for (Contact contact : itr) {
+                contacts.add(contact);
+            }
+            itr.close();
+        } catch (Exception e) {
+            Log.e("Contact List", "selectAllContacts: ", e);
+        }
+        return contacts;
+    }
+
+    public static Contact getRandomContact(SQLiteDatabase db) {
+        Random rand = new Random();
+        Contact contact = cupboard().withDatabase(db).get(Contact.class, rand.nextInt(20));
+        return contact;
+    }
+}
