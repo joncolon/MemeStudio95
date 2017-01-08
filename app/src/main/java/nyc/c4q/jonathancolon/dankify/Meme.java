22655d301cce8f6532c4d8d7cb72b21302d063ad
Jonathan Colon <jonatron2.0@gmail.com>
2017-01-08 17:57:43 -0500

    getters/setters

--- app/src/main/java/nyc/c4q/jonathancolon/dankify/Meme.java
+++ app/src/main/java/nyc/c4q/jonathancolon/dankify/Meme.java
@@ -5,8 +5,41 @@ package nyc.c4q.jonathancolon.dankify;
  */
 
 public class Meme {
+    Long _id;
     public String topText;
     public String bottomText;
     byte[] memeImage;
     String dateCreated;
+
+    public String getTopText() {
+        return topText;
+    }
+
+    public void setTopText(String topText) {
+        this.topText = topText;
+    }
+
+    public String getBottomText() {
+        return bottomText;
+    }
+
+    public void setBottomText(String bottomText) {
+        this.bottomText = bottomText;
+    }
+
+    public byte[] getMemeImage() {
+        return memeImage;
+    }
+
+    public void setMemeImage(byte[] memeImage) {
+        this.memeImage = memeImage;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
 }
