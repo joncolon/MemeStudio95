b5e393879bbc514814a7c3233ce6a4f3f9179313
catcattac <catwong.nyc@gmail.com>
2017-01-20 17:48:58 -0500

    added taskbar and intent to twitter activity

--- app/src/main/java/nyc/c4q/jonathancolon/dankify/MemeMain.java
+++ app/src/main/java/nyc/c4q/jonathancolon/dankify/MemeMain.java
@@ -1,10 +1,13 @@
 package nyc.c4q.jonathancolon.dankify;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import nyc.c4q.jonathancolon.dankify.twittermeme.activities.TweetActivity;
 
 /**
  * Created by catwong on 1/15/17.
@@ -22,7 +25,9 @@ public class MemeMain extends AppCompatActivity {
 
     }
 
-    public void startDrawStudio(View view){
-        //TODO fill in next intent -> DrawStudio activity
+    public void startDocuments(View view){
+        //TODO fill in next intent -> MyDocuments activity
+        Intent intent = new Intent(MemeMain.this, TweetActivity.class);
+        startActivity(intent);
     }
 }
